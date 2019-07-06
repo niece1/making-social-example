f198b20907560c2b20e00ebcd65885c610d177ca
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2019-07-06 17:40:10 +0300

    First init with some job done

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+           <img src="" class="rounded-circle w-100"> 
+        </div>
+        <div class="col-9 pt-5">
+
+        </div>
+    </div>
+</div>
+@endsection
