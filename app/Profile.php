04edb91ec9063d7567f26ce1c001e2ae147f1822
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2019-07-07 14:17:49 +0300

    Post, Profile models and controllers modified, Profile policy added

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+	protected $guarded = [];
+
+	public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/JNiNHZYPax0bk1mZWBDuZbvKfghk7OsZRJjsTrXO.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);  
--- app/User.php
+++ app/User.php
@@ -2,8 +2,11 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -34,10 +37,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+        protected static function boot()
+    {
+        parent::boot();
+        static::created(function ($user) {
+            $user->profile()->create([
+                'title' => $user->username,
+            ]);
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
+        });
+    }
+
     public function profile()
     {
         return $this->hasOne(Profile::class);
     }
+
     public function posts()
     {
         return $this->hasMany(Post::class)->orderBy('created_at', 'DESC');
