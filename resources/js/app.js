817e28f8b2dbac11178263e97b4955fd9be66653
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2021-12-16 00:00:35 +0200

    Basic project setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+window.Vue = require('vue').default;
+
+// For automatic component registration
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+const app = new Vue({
+    el: '#app'
+
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="gy">Hello</div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang='scss' scoped>
+.gy {
+    color: red;
+font-size: 30px;
+    
+}
+</style>
