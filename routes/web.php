d1a055430be7c9afc141948a9517c1e60ba700ac
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2021-12-15 15:33:27 +0200

    Initial set of files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,21 +13,6 @@
 |
 */
 
-use App\Mail\NewUserWelcomeMail;
-
-Auth::routes();
-
-Route::get('/email', function () {
-    return new NewUserWelcomeMail();
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::post('follow/{user}', 'FollowsController@store');
-
-Route::get('/', 'PostsController@index');
-Route::get('/p/create', 'PostsController@create');
-Route::post('/p', 'PostsController@store');
-Route::get('/p/{post}', 'PostsController@show');
-
-Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
-Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
-Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
