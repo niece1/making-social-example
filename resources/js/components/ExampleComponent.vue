4f31a5382f867281d8937b97bdaab9b9a9fac1ee
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2021-12-16 15:48:38 +0200

    :wrench: js, sanctum setup
    Javascript and sanctum setup

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,13 +1,27 @@
 <template>
     <div>
-        <div class="gy">Hello</div>
+        <div class="gy">Hello</div>{{ user }}
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
-};
+   data() {
+     return {
+       user: null
+}
+},
+methods: {
+      async  getUser() {
+let response = await axios.get('/api/user')
+this.user = response.data
+}
+},
+mounted() {
+    this.getUser()
+}
+}
 </script>
 
 <style lang='scss' scoped>
